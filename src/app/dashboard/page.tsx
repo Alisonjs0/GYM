f999ce8e6f0a87c7df2082e0c43b23d788a1ee14
Alisonjs0 <alisonserafim.tec@gmail.com>
2025-01-27 22:01:12 -0300

    .

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
-'use client'
+"use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { useLogin } from "@/hooks/useLogin";
 
@@ -12,36 +12,35 @@ import { TotalDeAlunos } from "@/components/TotaldeAlunos";
 import styles from "../styles/dashboard.module.css";
 
 const page = () => {
+  const { isLogged, redirect, hasRedirected } = useLogin();
 
-    const {isLogged, redirect, hasRedirected} = useLogin();
-
-    useEffect(() => {
-      if (isLogged === false && !hasRedirected) {
-        redirect();
-      }
-    }, [isLogged, hasRedirected, redirect]);
-
-    if (!isLogged) {
-      return (
-        <div className="w-full  h-full text-[#F4F4F5] m-auto flex flex-col justify-center items-center">
-          <p>Você precisa estar logado para acessar essa página.</p>
-          <p>Redirecionando para a pagina de login...</p>
-        </div>
-      );
+  useEffect(() => {
+    if (isLogged === false && !hasRedirected) {
+      redirect();
     }
+  }, [isLogged, hasRedirected, redirect]);
+
+  if (!isLogged) {
+    return (
+      <div className="w-full  h-full text-[#F4F4F5] m-auto flex flex-col justify-center items-center">
+        <p>Você precisa estar logado para acessar essa página.</p>
+        <p>Redirecionando para a pagina de login...</p>
+      </div>
+    );
+  }
 
   return (
     <div className={`${styles.container}`}>
       <h1 className={`text-[#F4F4F5] text-3xl my-12`}>Dashboard:</h1>
       <div className="grid grid-cols-5 grid-rows-8 gap-8">
         <span className="col-start-1 col-end-3 row-start-1 row-end-4 div1">
-        <TotalDeAlunos />
+          <TotalDeAlunos />
         </span>
         <span className="col-start-3 col-end-6 row-start-1 row-end-6 div2">
-        <CaptacaoDeAlunos />
+          <CaptacaoDeAlunos />
         </span>
         <span className="col-start-1 col-end-3 row-start-4 row-end-9 div3">
-        <ProgressoFinanceiro />
+          <ProgressoFinanceiro />
         </span>
       </div>
     </div>
