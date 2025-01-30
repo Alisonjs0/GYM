9e91a616a78a1213b0d40b3fbbd18015c7409043
Alisonjs0 <alisonserafim.tec@gmail.com>
2025-01-30 19:54:14 -0300

    ajuste no redirecionamento pra pagina de [UserID]

--- src/app/components/ListInfo.tsx
+++ src/app/components/ListInfo.tsx
@@ -1,9 +1,9 @@
-'use client'
+"use client";
 
 import { useState, useEffect } from "react";
 import { FaCircle } from "react-icons/fa";
 
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 export interface ListInfoProps {
   id: string;
@@ -12,6 +12,7 @@ export interface ListInfoProps {
   plano: string;
   status: string;
   className?: string;
+  children?: React.ReactNode;
 }
 
 export default function ListInfo({
@@ -21,42 +22,47 @@ export default function ListInfo({
   plano,
   status,
   className,
+  children,
 }: ListInfoProps) {
-    const [valor, setValor] = useState(false);
-    const [statusColor, setStatusColor] = useState("");
-    const router = useRouter();
+  const pathname = usePathname();
 
-    useEffect(() => {
-        handleStatus(status);
-      }, [status]);
+  const [valor, setValor] = useState(false);
+  const [statusColor, setStatusColor] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    handleStatus(status);
+  }, [status]);
 
   const handleStatus = (status: string) => {
     switch (status) {
       case "Ativo":
-        setValor(true)
+        setValor(true);
         setStatusColor("text-[#0FB800]");
-        break
+        break;
       case "Inativo":
-        setValor(true)
+        setValor(true);
         setStatusColor("text-[#C10003]");
-        break
+        break;
       case "Pendente":
-        setValor(true)
+        setValor(true);
         setStatusColor("text-[#C4CB00]");
-        break
+        break;
       case "Cancelado":
-        setValor(true)
+        setValor(true);
         setStatusColor("text-[#585858]");
-        break
+        break;
     }
   };
 
   const handleClick = () => {
-    router.push(`/alunos/${id}`);
-  }
+    if (pathname === `/alunos`) {
+      router.push(`/alunos/${id}`);
+    }
+  };
   return (
     <div
-      className={`${className} bg-[#232241] flex justify-between px-8 py-4 rounded-xl mb-4 drop-shadow-xl transition-transform hover:scale-105 cursor-pointer`}
+      className={`${className} bg-[#232241] flex justify-between px-8 py-4 rounded-xl mb-4 drop-shadow-xl cursor-pointer`}
       onClick={handleClick}
     >
       <p className="text-[#F4F4F5] w-1/2">{nome}</p>
@@ -65,7 +71,7 @@ export default function ListInfo({
       {!valor ? (
         <p className={`${statusColor} text-[#F4F4F5] w-1/6`}>{status}</p>
       ) : (
-        <span className={`${statusColor} w-1/6 pl-5`}>
+        <span className={`${statusColor} w-1/6 pl-5 mt-1`}>
           <FaCircle />
         </span>
       )}
