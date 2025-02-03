7da752b460b7cffb851701c58e7413acee669be2
Alisonjs0 <alisonserafim.tec@gmail.com>
2025-02-03 20:38:43 -0300

    melhoria da logica do navLayout

--- src/app/components/navLayout.tsx
+++ src/app/components/navLayout.tsx
@@ -5,22 +5,8 @@ import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function ClientWrapper() {
-  const [isLogged, setIsLogged] = useState<boolean | null>(null);
   const pathname = usePathname();
-  const hideNavBar = pathname === "/";
+  const hideNavBar = pathname === "/login";
 
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const storedIsLogged = sessionStorage.getItem("isLogged");
-      setIsLogged(storedIsLogged === "true");
-    }
-  }, [hideNavBar]);
-
-
-  if (isLogged === null) {
-    return null;
-  }
-
-  return <>{!hideNavBar && isLogged && <NavBar />}</>;
+  return <>{!hideNavBar && <NavBar />}</>;
 }
