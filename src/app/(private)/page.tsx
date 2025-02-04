da925db1edee4f98152ad0a7ec252e1ab3e112ca
Alisonjs0 <alisonserafim.tec@gmail.com>
2025-02-04 13:49:27 -0300

    pegando as informacoes direto do banco

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
-
-import { useLogin } from "@/hooks/useLogin";
+import React from "react";
 
 import { CaptacaoDeAlunos } from "@/components/CaptacaoDeAlunos";
 import { ProgressoFinanceiro } from "@/components/ProgressoFinanceiro";
@@ -11,7 +9,56 @@ import { TotalDeAlunos } from "@/components/TotaldeAlunos";
 //css
 import styles from "@/app/styles/dashboard.module.css";
 
+import { useFetchDocuments } from "@/hooks/useFetchDocuments";
+
+import { useRouter } from "next/navigation";
+
 const Dashboard = () => {
+  const router = useRouter();
+
+  const { documents: alunos, loading } = useFetchDocuments("alunos");
+
+  const alunosAtivos =
+    alunos && alunos.filter((aluno) => aluno.status === "Ativo");
+  const alunosPendentes =
+    alunos && alunos.filter((aluno) => aluno.status === "Pendente");
+
+  const meses = [
+    "JANEIRO",
+    "FEVEREIRO",
+    "MARCO",
+    "ABRIL",
+    "MAIO",
+    "JUNHO",
+    "JULHO",
+    "AGOSTO",
+    "SETEMBRO",
+    "OUTUBRO",
+    "NOVEMBRO",
+    "DEZEMBRO",
+  ];
+
+  const contagemPorMes = new Array(12).fill(0);
+  const desistenciasPorMes = new Array(12).fill(0);
+
+  alunos?.forEach((aluno) => {
+    if (aluno.createdAt) {
+      const dataCriacao = aluno.createdAt.toDate();
+      const mes = dataCriacao.getMonth();
+      contagemPorMes[mes]++;
+      if (!aluno.ativo) {
+        desistenciasPorMes[mes]++;
+      }
+    }
+  });
+
+  const resultdado = meses.map((nomeMes, index) => ({
+    mes: nomeMes,
+    quantidade: contagemPorMes[index],
+    desistencias: desistenciasPorMes[index],
+  }));
+
+  console.log(resultdado)
 
   return (
     <div className={`${styles.container}`}>
@@ -20,22 +67,23 @@ const Dashboard = () => {
         <span className="w-[32%] flex flex-col gap-y-8">
           <span>
             <TotalDeAlunos
-              total={50}
+              total={alunosAtivos?.length || 0}
               title="Total de Alunos Ativos"
               subtitle="Alunos Ativos"
             />
           </span>
           <span>
             <TotalDeAlunos
-              total={23}
+              total={alunosPendentes?.length || 0}
               title="Pagamentos Pendentes"
               subtitle="Pagamentos Pendentes"
+              handleRedirect={() => router.push("/financeiro")}
             />
           </span>
         </span>
         <span className="w-[65%] flex flex-col gap-y-8 pb-12">
           <span>
-            <CaptacaoDeAlunos />
+            <CaptacaoDeAlunos result={resultdado} />
           </span>
           <span>
             <ProgressoFinanceiro />
