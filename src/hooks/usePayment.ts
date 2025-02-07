aee63b62ed54159ffd4c12ddeaa8d1c661a81a55
Alisonjs0 <alisonserafim.tec@gmail.com>
2025-02-06 21:04:41 -0300

    melhoria no processamento de pagamentos

--- src/hooks/usePayment.ts
+++ src/hooks/usePayment.ts
@@ -1,43 +1,53 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export function usePayment() {
-    const [valor, setValor] = useState(0);
-    const [nome, setNome] = useState('');
-    const [plano, setPlano] = useState('');
-    const [qrCode, setQrCode] = useState('');
+  const [valor, setValor] = useState(0);
+  const [nome, setNome] = useState("");
+  const [plano, setPlano] = useState("");
+  const [qrCode, setQrCode] = useState("");
+  const isProcessing = useRef(false);
 
-    const handleChangeInfo = (valor: number, nome: string, plano: string ) => {
-        setValor(valor);
-        setNome(nome);
-        setPlano(plano);
-        processarPagamento();
-      };
+  const handleChangeInfo = (valor: number, nome: string, plano: string) => {
+    setValor(valor);
+    setNome(nome);
+    setPlano(plano);
+  };
 
-    useEffect(() => {
-        if (qrCode) {
-            window.open(qrCode);
-        }
-    }, [qrCode]);
+  useEffect(() => {
+    if (valor > 0 && nome && plano && !isProcessing.current) {
+      processarPagamento();
+    }
+  }, [valor, nome, plano]);
 
-    const processarPagamento = useCallback(async () => {
-        try {
-          const response = await fetch('/api/mercadopago', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ valor, nome, plano }),
-          });
-    
-          if (!response.ok) {
-            throw new Error('Erro ao processar o pagamento');
-          }
-          const data = await response.json();
-          setQrCode(`${data.qrcode}`);
-        } catch (error) {
-          console.log('Erro ao processar o pagamento:', error);
-        }
-      }, [valor, nome, plano]);
+  const processarPagamento = async () => {
+    if (isProcessing.current) return;
+    isProcessing.current = true;
 
-        return {processarPagamento, qrCode, handleChangeInfo};
-    };
+    try {
+      const response = await fetch("/api/mercadopago", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ valor, nome, plano }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Erro ao processar o pagamento");
+      }
+      const data = await response.json();
+      setQrCode(`${data.qrcode}`);
+    } catch (error) {
+      console.log("Erro ao processar o pagamento:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (qrCode) {
+      const win = window.open(qrCode, "_blank");
+      if (win) win.focus();
+    }
+  })
+
+  return { processarPagamento, qrCode, handleChangeInfo };
+}
